391df5b2a6e0619dddeefc860da625f9cbfe5abb
Thonku971 <99925563+Thonku971@users.noreply.github.com>
2023-06-20 00:51:42 -0300

    Update JanelaInicial.java

--- src/gui/JanelaInicial.java
+++ src/gui/JanelaInicial.java
@@ -1,20 +1,30 @@
 package gui;
 
+import java.awt.Component;
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JFormattedTextField;
 import javax.swing.JPasswordField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.sql.SQLException;
 import java.awt.event.ActionEvent;
+
+import service.DespesasOcasionaisService;
+import service.UsuarioService;
+import entity.Usuario;
 
 public class JanelaInicial {
 
+	private static UsuarioService usuarioService;
 	private JFrame frmInicio;
 	private JTextField textUsuario;
 	private JPasswordField textSenha;
+	private Usuario s;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -27,20 +37,23 @@ public class JanelaInicial {
 				}
 			}
 		});
+
+		
 	}
 
-	public JanelaInicial() {
+
+		public JanelaInicial() {
 		initialize();
 	}
 
 	private void initialize() {
 		frmInicio = new JFrame();
-		frmInicio.setTitle("InÌcio");
+		frmInicio.setTitle("In√≠cio");
 		frmInicio.setBounds(100, 100, 250, 210);
 		frmInicio.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmInicio.getContentPane().setLayout(null);
 		
-		JLabel lblUsuario = new JLabel("Usu·rio");
+		JLabel lblUsuario = new JLabel("Usu√°rio");
 		lblUsuario.setBounds(30, 30, 46, 14);
 		frmInicio.getContentPane().add(lblUsuario);
 		
@@ -60,9 +73,25 @@ public class JanelaInicial {
 		JButton btnLogin = new JButton("Login");
 		btnLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				/* ValidaÁ„o de login */
-				gui.JanelaRendimentos.main(null);
-				frmInicio.dispose();
+				Usuario s = montaUsuario();  
+				usuarioService = new UsuarioService();
+				
+				try {
+					 s.setLog(usuarioService.loginUsuario(s));  
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					Component frame = null;
+					JOptionPane.showMessageDialog(frame,"ERROR");
+				} 
+			
+				                  JOptionPane.showMessageDialog(null, "Usuario cadastrado.");  
+				                  limpaTela();  
+				/* Valida√ß√£o de login */
+
+				          		System.out.println("AAAasaA " + s.getId());
+				        		frmInicio.dispose();
+				          		gui.JanelaRendimentos.main(s);
+		
 			}
 		});
 		btnLogin.setBounds(60, 90, 120, 23);
@@ -71,12 +100,45 @@ public class JanelaInicial {
 		JButton btnCadastrar = new JButton("Cadastrar");
 		btnCadastrar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				 s = montaUsuario();  
+				usuarioService = new UsuarioService();
+				
+				try {
+					 s.setId(usuarioService.cadastrarUsuario(s));  
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					Component frame = null;
+					JOptionPane.showMessageDialog(frame,"ERROR");
+				} 
+			
+				                  JOptionPane.showMessageDialog(null, "Usuario cadastrado.");  
+				                  limpaTela();  
+		
 				/* Verifica se usuario existe e cadastra */
-				gui.JanelaRendimentos.main(null);
-				frmInicio.dispose();
 			}
 		});
 		btnCadastrar.setBounds(60, 120, 120, 23);
-		frmInicio.getContentPane().add(btnCadastrar);
-	}
+		frmInicio.getContentPane().add(btnCadastrar);}
+		
+		
+		  public void limpaTela() {  
+              this.textUsuario.setText("");  
+              this.textSenha.setText("");  
+            }  
+      
+           
+      
+         @SuppressWarnings("deprecation")
+		public Usuario montaUsuario() { // Pega dos dados digitados nos campos do  
+              // formul√°rio e atribui ao objeto  
+        	 Usuario lg = new Usuario();  
+              lg.setLogin(this.textUsuario.getText());  
+                
+              lg.setSenha(this.textSenha.getText());  
+      
+              return lg;  
+      
+         }  
+      
+     
 }
