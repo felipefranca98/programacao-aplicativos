78b820979408b1e3e64faa01a97a163fdb27f2a2
Thonku971 <99925563+Thonku971@users.noreply.github.com>
2023-06-20 00:50:56 -0300

    Update JanelaDespesas.java

--- src/gui/JanelaDespesas.java
+++ src/gui/JanelaDespesas.java
@@ -1,11 +1,15 @@
+
 package gui;
 
 import java.awt.EventQueue;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.TitledBorder;
 import javax.swing.border.EtchedBorder;
 import java.awt.Color;
+import java.awt.Component;
+
 import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
 import javax.swing.SwingConstants;
@@ -16,12 +20,28 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
+import entity.Despesas;
+import entity.Usuario;
+import service.DespesasService;
+import service.UsuarioService;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JanelaDespesas {
 
+	private static DespesasService despesaService;
+	private static UsuarioService usuarioService = new UsuarioService();
+	private JTable tabela;
+	private DefaultTableModel modelo = new DefaultTableModel();
+	private static  Usuario user = new Usuario();
 	private JFrame frmDespesas;
-	private JTable table;
 
-	public static void main(String[] args) {
+	public static void main(Usuario s) {
+		user.setId(s.getId());
+		user.setLogin(s.getLogin());
+		user.setSenha(s.getSenha());
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -35,6 +55,8 @@ public class JanelaDespesas {
 	}
 
 	public JanelaDespesas() {
+		criaJTable();
+		
 		initialize();
 	}
 
@@ -55,15 +77,8 @@ public class JanelaDespesas {
 		scrollPane.setBounds(10, 24, 574, 395);
 		panel.add(scrollPane);
 		
-		table = new JTable();
-		table.setModel(new DefaultTableModel(
-			new Object[][] {
-			},
-			new String[] {
-				"Categoria", "Despesa", "Mensal", "Ocasional", "Total Anual"
-			}
-		));
-		scrollPane.setViewportView(table);
+		scrollPane.setViewportView(tabela);
+
 		
 		JSeparator separator = new JSeparator();
 		separator.setOrientation(SwingConstants.VERTICAL);
@@ -79,7 +94,7 @@ public class JanelaDespesas {
 		JButton btnCadastrarCat = new JButton("<html><center>Cadastrar<br>Categoria</center>");
 		btnCadastrarCat.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				gui.JanelaCadastrarCategorias.main(null, 2);
+				gui.JanelaCadastrarCategorias.main(null, 2, user);
 				frmDespesas.dispose();
 			}
 		});
@@ -110,7 +125,7 @@ public class JanelaDespesas {
 		JButton btnCadastrarDesp = new JButton("<html><center>Cadastrar<br>Despesa</center>");
 		btnCadastrarDesp.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				gui.JanelaCadastrarDespesas.main(null);
+				gui.JanelaCadastrarDespesas.main(user);
 				frmDespesas.dispose();
 			}
 		});
@@ -140,7 +155,7 @@ public class JanelaDespesas {
 		JButton btnRendimentos = new JButton("Rendimentos");
 		btnRendimentos.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				gui.JanelaRendimentos.main(null);
+				gui.JanelaRendimentos.main(user);
 				frmDespesas.dispose();
 			}
 		});
@@ -154,7 +169,7 @@ public class JanelaDespesas {
 		JButton btnInvestimentos = new JButton("Investimentos");
 		btnInvestimentos.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				gui.JanelaInvestimentos.main(null);
+				gui.JanelaInvestimentos.main(user);
 				frmDespesas.dispose();
 			}
 		});
@@ -163,7 +178,7 @@ public class JanelaDespesas {
 		JButton btnFundos = new JButton("Fundos de Despesas");
 		btnFundos.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				gui.JanelaFundoDespesas.main(null);
+				gui.JanelaFundoDespesas.main(user);
 				frmDespesas.dispose();
 			}
 		});
@@ -172,7 +187,7 @@ public class JanelaDespesas {
 		JButton btnResumos = new JButton("Resumos");
 		btnResumos.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				gui.JanelaResumos.main(null);
+				gui.JanelaResumos.main(user);
 				frmDespesas.dispose();
 			}
 		});
@@ -188,11 +203,58 @@ public class JanelaDespesas {
 		JButton btnLogout = new JButton("Logout");
 		btnLogout.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				try {
+					 user.setLog(usuarioService.logoutUsuario(user));  
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					Component frame = null;
+					JOptionPane.showMessageDialog(frame,"ERROR");
+				} 
+
 				gui.JanelaInicial.main(null);
 				frmDespesas.dispose();
 			}
 		});
 		btnLogout.setBounds(625, 11, 149, 57);
 		frmDespesas.getContentPane().add(btnLogout);
+	}
+	
+	private void criaJTable() {
+		tabela = new JTable(modelo);
+		modelo.addColumn("Categoria");
+		modelo.addColumn("Despesa");
+		modelo.addColumn("Mensal");
+		modelo.addColumn("Ocasional");
+		modelo.addColumn("Total Anual");
+		tabela.getColumnModel().getColumn(0)
+		.setPreferredWidth(120);
+		tabela.getColumnModel().getColumn(1)
+		.setPreferredWidth(120);
+		tabela.getColumnModel().getColumn(1)
+		.setPreferredWidth(80);
+		tabela.getColumnModel().getColumn(1)
+		.setPreferredWidth(80);
+		tabela.getColumnModel().getColumn(1)
+		.setPreferredWidth(80);
+		pesquisar(modelo);
+	}
+
+	public static void pesquisar(DefaultTableModel modelo) {
+		despesaService = new DespesasService();
+		modelo.setNumRows(0);
+		List <Despesas> despesa= new ArrayList<>();
+		int a =1;
+		try {
+			System.out.println("\t Buscar Investimento");
+			despesa = despesaService.buscarDespesa(user.getId());
+		} catch (Exception e){
+			Component frame = null;
+			JOptionPane.showMessageDialog(frame,"ERROR");}
+			
+
+		for (Despesas despesa1 : despesa) {
+			modelo.addRow(new Object[]{despesa1.getCategoria(), despesa1.getDespesa(),
+					despesa1.getMensal(), despesa1.getOcasional(), despesa1.getTotal()});
+		}
 	}
 }
