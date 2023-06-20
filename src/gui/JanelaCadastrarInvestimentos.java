4ca13c5d42eb1820fd98b8a604778a44909de7f2
Thonku971 <99925563+Thonku971@users.noreply.github.com>
2023-06-20 00:50:21 -0300

    Update JanelaCadastrarInvestimentos.java

--- src/gui/JanelaCadastrarInvestimentos.java
+++ src/gui/JanelaCadastrarInvestimentos.java
@@ -1,6 +1,7 @@
-package gui;
+/*package gui;
 
 import java.awt.EventQueue;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JComboBox;
@@ -14,6 +15,7 @@ import javax.swing.SwingConstants;
 import javax.swing.JButton;
 import javax.swing.ButtonGroup;
 import javax.swing.DefaultComboBoxModel;
+
 import java.awt.event.ActionListener;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -90,7 +92,8 @@ public class JanelaCadastrarInvestimentos {
 		frmCadastrarInvestimentos.getContentPane().add(textValor);
 		
 		JButton btnCadastrar = new JButton("Cadastrar");
-		btnCadastrar.setBounds(214, 93, 160, 23);
+		btnCadastrar.setEnabled(false);
+		btnCadastrar.setBounds(214, 96, 160, 23);
 		frmCadastrarInvestimentos.getContentPane().add(btnCadastrar);
 		
 		JButton btnCancelar = new JButton("Cancelar");
@@ -103,7 +106,7 @@ public class JanelaCadastrarInvestimentos {
 		btnCancelar.setBounds(214, 127, 160, 23);
 		frmCadastrarInvestimentos.getContentPane().add(btnCancelar);
 		
-		JLabel lblMesAno = new JLabel("M�s/Ano");
+		JLabel lblMesAno = new JLabel("Mês/Ano");
 		lblMesAno.setBounds(10, 42, 65, 14);
 		frmCadastrarInvestimentos.getContentPane().add(lblMesAno);
 		
@@ -130,5 +133,269 @@ public class JanelaCadastrarInvestimentos {
 		cbAno.setBounds(154, 39, 65, 22);
 		frmCadastrarInvestimentos.getContentPane().add(cbAno);
 	}
+
+}
+*/
+
+package gui;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JComboBox;
+import javax.swing.JTextField;
+import javax.swing.JPanel;
+import javax.swing.border.TitledBorder;
+
+import entity.DespesasOcasionais;
+import entity.Investimento;
+import entity.Usuario;
+import service.DespesasOcasionaisService;
+import service.InvestimentoService;
+
+import javax.swing.border.EtchedBorder;
+import java.awt.Color;
+import java.awt.Component;
+
+import javax.swing.JRadioButton;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import javax.swing.ButtonGroup;
+import javax.swing.DefaultComboBoxModel;
+import java.awt.event.ActionListener;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+
+public class JanelaCadastrarInvestimentos {
+
+	private JFrame frmCadastrarInvestimentos;
+	private JTextField textInvestimento;
+	private JTextField textValor;
+	private final ButtonGroup buttonGroup = new ButtonGroup();
+	private String mes;
+	private String ano;
+	private boolean frequencia;
+	private Investimento investimento = new Investimento();
+	private static InvestimentoService investimentoService;
+	
+	private static  Usuario user = new Usuario();
+	public static void main(Usuario s) {
+		user.setId(s.getId());
+		user.setLogin(s.getLogin());
+		user.setSenha(s.getSenha());
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JanelaCadastrarInvestimentos window = new JanelaCadastrarInvestimentos();
+					window.frmCadastrarInvestimentos.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public JanelaCadastrarInvestimentos() {
+		initialize();
+	}
+
+	private void initialize() {
+		frmCadastrarInvestimentos = new JFrame();
+		frmCadastrarInvestimentos.setTitle("Cadastrar Investimentos");
+		frmCadastrarInvestimentos.setBounds(100, 100, 400, 200);
+		frmCadastrarInvestimentos.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmCadastrarInvestimentos.getContentPane().setLayout(null);
+		
+		JLabel lblInvestimento = new JLabel("Investimento");
+		lblInvestimento.setBounds(10, 14, 80, 14);
+		frmCadastrarInvestimentos.getContentPane().add(lblInvestimento);
+		
+		textInvestimento = new JTextField();
+		textInvestimento.setColumns(10);
+		textInvestimento.setBounds(100, 11, 274, 20);
+		frmCadastrarInvestimentos.getContentPane().add(textInvestimento);
+		
+		JPanel panelTipo = new JPanel();
+		panelTipo.setLayout(null);
+		panelTipo.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "Tipo de Investimento", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		panelTipo.setBounds(10, 70, 175, 80);
+		frmCadastrarInvestimentos.getContentPane().add(panelTipo);
+		
+		JRadioButton rdbtnOcasional = new JRadioButton("Ocasional");
+		buttonGroup.add(rdbtnOcasional);
+		rdbtnOcasional.setSelected(true);
+		rdbtnOcasional.setHorizontalAlignment(SwingConstants.CENTER);
+		rdbtnOcasional.setBounds(6, 19, 163, 23);
+		panelTipo.add(rdbtnOcasional);
+		rdbtnOcasional.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frequencia = false;
+
+
+		        System.out.println("Ocasional" + frequencia);
+			}
+		});
+		
+		JRadioButton rdbtnMensal = new JRadioButton("Mensal");
+		buttonGroup.add(rdbtnMensal);
+		rdbtnMensal.setHorizontalAlignment(SwingConstants.CENTER);
+		rdbtnMensal.setBounds(6, 45, 163, 23);
+		panelTipo.add(rdbtnMensal);
+		rdbtnMensal.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frequencia = true;
+
+		        System.out.println("Mensal   " + frequencia);
+			}
+		});
+		
+		JLabel lblValor = new JLabel("Valor");
+		lblValor.setBounds(254, 42, 50, 14);
+		frmCadastrarInvestimentos.getContentPane().add(lblValor);
+		
+		textValor = new JTextField();
+		textValor.setColumns(10);
+		textValor.setBounds(294, 39, 80, 20);
+		frmCadastrarInvestimentos.getContentPane().add(textValor);
+		
+		JButton btnCadastrar = new JButton("Cadastrar");
+		btnCadastrar.setBounds(214, 93, 160, 23);
+		frmCadastrarInvestimentos.getContentPane().add(btnCadastrar);
+		btnCadastrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				investimento = montaInvestimento();  
+				investimentoService = new InvestimentoService();
+				
+				try {
+					 investimentoService.cadastrarInvestimento(investimento);  
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					Component frame = null;
+					JOptionPane.showMessageDialog(frame,"ERROR");
+				} 
+			
+				                  JOptionPane.showMessageDialog(null, "Despesa cadastrado.");  
+				                  limpaTela();  
+			
+				gui.JanelaInvestimentos.main(user);
+				frmCadastrarInvestimentos.dispose();
+			}
+		});
+		
+		JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				gui.JanelaInvestimentos.main(user);
+				frmCadastrarInvestimentos.dispose();
+			}
+		});
+		btnCancelar.setBounds(214, 127, 160, 23);
+		frmCadastrarInvestimentos.getContentPane().add(btnCancelar);
+		
+		JLabel lblMesAno = new JLabel("Mês/Ano");
+		lblMesAno.setBounds(10, 42, 65, 14);
+		frmCadastrarInvestimentos.getContentPane().add(lblMesAno);
+		
+		LocalDate data = LocalDate.now();
+		Locale local = new Locale("pt", "BR");
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("MMM", local);
+		
+		JComboBox cbMes = new JComboBox();
+		cbMes.setModel(new DefaultComboBoxModel(new String[] {"JAN", "FEV", "MAR", "ABR", "MAI", "JUN", "JUL", "AGO", "SET", "OUT", "NOV", "DEZ"}));
+		cbMes.setSelectedItem(fmt.format(data).toUpperCase());
+		cbMes.setBounds(69, 39, 65, 22);
+		frmCadastrarInvestimentos.getContentPane().add(cbMes);
+		cbMes.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				    mes = (String)cbMes.getSelectedItem();
+			        System.out.println("Mes   " + mes);
+			}
+		});
+		
+		JLabel lblBarra = new JLabel("/");
+		lblBarra.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblBarra.setBounds(141, 42, 22, 14);
+		frmCadastrarInvestimentos.getContentPane().add(lblBarra);
+		
+		fmt = DateTimeFormatter.ofPattern("YYYY", local);
+		
+		JComboBox cbAno = new JComboBox();
+		cbAno.setModel(new DefaultComboBoxModel(new String[] {"2023", "2022", "2021", "2020", "2019", "2018", "..."}));
+		cbAno.setSelectedItem(fmt.format(data));
+		cbAno.setBounds(154, 39, 65, 22);
+		frmCadastrarInvestimentos.getContentPane().add(cbAno);
+		cbAno.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				    ano = (String)cbAno.getSelectedItem();
+			        System.out.println("Ano   " + ano);
+			}
+		});
+	}
+	
+	 public void limpaTela() {  
+		  this.textInvestimento.setText("");
+         
+         this.textValor.setText("");  
+       }  
+ 
+      
+ 
+	public Investimento montaInvestimento() { // Pega dos dados digitados nos campos do  
+         // formulário e atribui ao objeto  
+   	 Investimento lg = new Investimento();  
+         lg.setUser(user.getId());  
+         lg.setAno(Integer.parseInt(ano));
+
+         lg.setInvestimento(this.textInvestimento.getText());
+         if(frequencia == true)
+         {
+       	  lg.setMensal(Double.parseDouble(this.textValor.getText()));
+       	  lg.setOcasional(0.0);
+
+         }
+         if(frequencia == false)
+         {
+       	  lg.setOcasional(Double.parseDouble(this.textValor.getText()));
+       	  lg.setMensal(0.0);
+         }
+         
+         switch(mes) {
+         case "JAN":
+       	  lg.setMes(1);
+         case "FEV":
+       	  lg.setMes(2);
+         case "MAR":
+       	  lg.setMes(3);
+         case "ABR":
+       	  lg.setMes(4);
+         case "MAI":
+       	  lg.setMes(5);
+         case "JUN":
+       	  lg.setMes(6);
+         case "JUL":
+       	  lg.setMes(7);
+         case "AGO":
+       	  lg.setMes(8);
+         case "SET":
+       	  lg.setMes(9);
+         case "OUT":
+       	  lg.setMes(10);
+         case "NOV":
+       	  lg.setMes(11);
+         case "DEZ":
+       	  lg.setMes(12);
+         }
+
+         return lg;  
+ 
+    }  
+	
+
+	
 
 }
