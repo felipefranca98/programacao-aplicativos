1a3844ddab6162703629152a08fecdcac090ac83
Thonku971 <99925563+Thonku971@users.noreply.github.com>
2023-06-20 00:49:24 -0300

    Update JanelaCadastrarCategorias.java

--- src/gui/JanelaCadastrarCategorias.java
+++ src/gui/JanelaCadastrarCategorias.java
@@ -1,21 +1,35 @@
+
 package gui;
 
+import java.awt.Component;
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+
+import entity.Tipo;
+import entity.Usuario;
+import service.TipoService;
+import service.UsuarioService;
+
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class JanelaCadastrarCategorias {
 
+public class JanelaCadastrarCategorias {
+	private static TipoService tipoService;
 	private JFrame frmCadastrarCategorias;
 	private JTextField textCategoria;
-	
-	private static int origem = 0;
+	private static  Usuario user = new Usuario();
 
-	public static void main(String[] args, int flag) {
+	private static int origem;
+
+	public static void main(String[] args, int flag, Usuario s) {
+		user.setId(s.getId());
+		user.setLogin(s.getLogin());
+		user.setSenha(s.getSenha());
 		origem = flag;
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -54,11 +68,11 @@ public class JanelaCadastrarCategorias {
 			public void actionPerformed(ActionEvent e) {
 				switch(origem) {
 				case 1:
-					gui.JanelaRendimentos.main(null);
+					gui.JanelaRendimentos.main(user);
 					frmCadastrarCategorias.dispose();
 					break;
 				case 2:
-					gui.JanelaDespesas.main(null);
+					gui.JanelaDespesas.main(user);
 					frmCadastrarCategorias.dispose();
 					break;
 				default:
@@ -71,8 +85,70 @@ public class JanelaCadastrarCategorias {
 		frmCadastrarCategorias.getContentPane().add(btnCancelar);
 		
 		JButton btnCadastrar = new JButton("Cadastrar");
+		btnCadastrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				tipoService = new TipoService();
+				Tipo categoria = new Tipo();
+				switch(origem) {
+				case 1:
+					categoria = montaTipo("Rendimento");
+					
+					try {
+						 tipoService.cadastrarCategoria(categoria);  
+					} catch (Exception e1) {
+						// TODO Auto-generated catch block
+						Component frame = null;
+						JOptionPane.showMessageDialog(frame,"ERROR");
+					} 
+				
+					                  JOptionPane.showMessageDialog(null, "Usuario cadastrado.");  
+					                  limpaTela();  
+			
+					gui.JanelaRendimentos.main(user);
+					frmCadastrarCategorias.dispose();
+					break;
+				case 2:
+					categoria = montaTipo("Despesa");
+					try {
+						 tipoService.cadastrarCategoria(categoria);  
+					} catch (Exception e1) {
+						// TODO Auto-generated catch block
+						Component frame = null;
+						JOptionPane.showMessageDialog(frame,"ERROR");
+					} 
+				
+					                  JOptionPane.showMessageDialog(null, "Usuario cadastrado.");  
+					                  limpaTela();  
+
+					gui.JanelaDespesas.main(user);
+					frmCadastrarCategorias.dispose();
+					break;
+				default:
+					frmCadastrarCategorias.dispose();
+					break;
+				}
+			}
+		});
+		
 		btnCadastrar.setBounds(150, 36, 100, 23);
 		frmCadastrarCategorias.getContentPane().add(btnCadastrar);
 	}
+	
+	public void limpaTela() {  
+        this.textCategoria.setText("");  
+      }  
+
+     
+
+	public Tipo montaTipo(String modulo) { // Pega dos dados digitados nos campos do  
+        // formulário e atribui ao objeto  
+  	 Tipo lg = new Tipo();  
+        lg.setNome(this.textCategoria.getText());  
+          
+        lg.setModulo(modulo);  
+
+        return lg;  
+
+   }  
 
 }
